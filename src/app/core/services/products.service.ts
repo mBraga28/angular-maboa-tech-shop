fcc4395e28f14b201b5e68e35140f7c5d05602fb
João Pedro de Morais Ferreira <jpmferreira96@gmail.com>
2024-01-18 00:12:39 -0300

    Adding a method to update inventory quantity after buying items

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -24,4 +24,22 @@ export class ProductsService {
   getOne(productId: number) {
     return this.products.find(product => product.id === productId);
   }
+
+  updateInventoryQuantity(newQuantity: number, productId: number){
+    let productToUpdate = this.getOne(productId);
+    if(productToUpdate) {
+
+      // Copying the products array
+      const updatedProducts = [...this.products];
+      // Updating the inventory quantity
+      productToUpdate!.inventoryQuantity = newQuantity;
+
+      // Getting the position of the product to be updated on the array
+      const oldProductIndex = updatedProducts.findIndex(p => p.id === productId);
+      updatedProducts[oldProductIndex] = productToUpdate!;
+
+      // Replacing the old array with the new and updated product list
+      this.products = updatedProducts;
+    }
+  }
 }
